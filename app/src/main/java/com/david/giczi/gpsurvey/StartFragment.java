e688becab75de38dcc60d6105ba2494d3beb15a2
david.giczi@gmail.com <david.giczi@gmail.com>
2024-01-31 08:55:07 +0100

    BackgroundImage

--- app/src/main/java/com/david/giczi/gpsurvey/StartFragment.java
+++ app/src/main/java/com/david/giczi/gpsurvey/StartFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
 
 import com.david.giczi.gpsurvey.databinding.FragmentStartBinding;
